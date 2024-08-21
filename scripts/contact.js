4112607dfccc34d79d89a820b725697c98ada0e6
Matissoss <156523002+Matissoss@users.noreply.github.com>
2024-08-21 12:02:46 +0200

    Last Page rework (i hope so)

--- scripts/contact.js
+++
@@ -1,6 +0,0 @@
-function mail(){
-    window.open("mailto:matissossGameDev@proton.me");
-}
-function twitterMsg(){
-    window.open("https://twitter.com/messages/compose?recipient_id=1774099156438532096");
-}
---
+++ scripts/sites.js
@@ -0,0 +1,15 @@
+function itchio(){
+    window.open("https://matissoss.itch.io");
+}
+function github(){
+    window.open("https://github.com/matissoss");
+}
+function twitter(){
+    window.open("https://x.com/@matissossgdev");
+}
+function youtube(){
+    window.open("https://www.youtube.com/@MatissossGameDev");
+}
+function soundcloud(){
+    window.open("https://soundcloud.com/matissossgamedev");
+}
